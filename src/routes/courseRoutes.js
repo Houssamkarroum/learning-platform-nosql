8664aa0e07fcecc53126c174cf9f7a00037d62ab
Houssamkarroum <karroumhoussam2020@gmail.com>
2025-01-06 14:56:31 +0100

    h

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -10,6 +10,7 @@ const courseController = require('../controllers/courseController');
 // Routes pour les cours
 router.post('/', courseController.createCourse);
 router.get('/stats', courseController.getCourseStats);
+
 router.get('/:id', courseController.getCourse);
 
 
